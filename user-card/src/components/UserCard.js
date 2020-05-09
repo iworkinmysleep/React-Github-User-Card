cec54a912a10c01d0e9c276ea8fdd324f608f52f
Jason Hinton <jason_hinton@att.net>
2020-05-08 23:44:54 -0400

    added card

--- user-card/src/components/UserCard.js
+++ user-card/src/components/UserCard.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class UserCard extends Component {
-  render() {
-    return (
-      <div>
-        <h1>User Card App</h1>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div class="card">
+				<img src="" />
+				<div class="card-info">
+					<h3 class="name">user's name</h3>
+					<p class="username">user name</p>
+					<p>Location:</p>
+					<p>
+						Profile:
+						<a href="#">address</a>
+					</p>
+					<p>Followers:</p>
+					<p>Following:</p>
+					<p>Bio:</p>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default UserCard;
